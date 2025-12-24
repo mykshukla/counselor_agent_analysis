7539fe744c7087bd9b7448a4bfbcb08713d73f12
mykshukla <dev@seriindia.org>
2025-12-24 14:32:36 +0530

    second key

--- app/llm/openai_client.py
+++ app/llm/openai_client.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import os
 
-client = OpenAI("")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def openai_chat(system: str, user: str, temperature: float = 0.3) -> str:
     resp = client.chat.completions.create(
